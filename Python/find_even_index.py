6d21452230f20e759765afc826627266cd35b499
ElHassen <Boumeddiene.elhassen@gmail.com>
2022-07-28 05:35:17 +0100

    new challenge
    find even index in a list

--- Python/find_even_index.py
+++ Python/find_even_index.py
@@ -1,40 +1,23 @@
+# You are going to be given an array of integers. 
+# Your job is to take that array and find an index 
+# N where the sum of the integers to the left of N 
+# is equal to the sum of the integers to the right 
+# of N. If there is no index that would make this 
+# happen, return -1.
+
 import math
-
-
-arr = list(range(1,100))
-
-
-
-
-   
-# for i in range(0,len(arr)):
-#     print(f'--------i = {i}')
-#     print(f'array is  {arr}')
-#     index = i
-#     print(f'index is  {index}')
-#     begin = 0
-#     print(f'begin  is  {begin}')
-#     end = len(arr) 
-#     print(f'end is  {end}')
-#     right = arr[i+1:end]
-#     print(f'right list is {right}')
-#     print(f'math.fsum of right list is {int(math.fsum(right))}')
-#     left = arr[begin:i]
-#     print(f'left list is {left}')
-#     print(f'math.fsum of left list is {math.fsum(left)}')
-even_state = False
-
-for i in range(0,len(arr)):
-    begin = 0
-    end = len(arr) 
-    right = arr[i+1:end]
-    left = arr[begin:i]
-    if math.fsum(left) == math.fsum(right):
-        if even_state == True :
-            break
-        even_index = i
-        even_state = True
-    elif i == len(arr) - 1 and even_state == False :
-        even_index = -1
-
-print(even_index)
+def find_even_index(arr):
+    even_state = False
+    for i in range(0,len(arr)):
+        begin = 0
+        end = len(arr) 
+        right = arr[i+1:end]
+        left = arr[begin:i]
+        if math.fsum(left) == math.fsum(right):
+            if even_state == True :
+                break
+            even_index = i
+            even_state = True
+        elif i == len(arr) - 1 and even_state == False :
+            even_index = -1
+    return even_index
