2e97595198cab6dec2bfd07fe93a9ca72d6c1353
ElHassen <Boumeddiene.elhassen@gmail.com>
2022-07-31 06:40:20 +0100

    new challenge
    check peaks in an array

---
+++ Python/pick_peaks.py
@@ -0,0 +1,19 @@
+
+def pick_peaks(arr):
+    dictio = {
+        "pos" : [],
+        "peaks" : []
+    }
+
+    posmax = 0
+    is_greater = False
+    for i in range(1,len(arr)):
+        if arr[i] > arr[i-1] :
+            is_greater = True
+            posmax = i
+        elif arr[i] < arr[i-1] and is_greater:
+            is_greater = False
+            dictio['pos'].append(posmax)
+            dictio['peaks'].append(arr[posmax])
+            
+    return dictio
