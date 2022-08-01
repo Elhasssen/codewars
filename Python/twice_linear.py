27ad5597acdcfd5ad5aff65f830e7413ca487f28
ElHassen <Boumeddiene.elhassen@gmail.com>
2022-08-01 09:08:50 +0100

    new challenge
    linear sequence

--- Python/twice_linear.py
+++ Python/twice_linear.py
@@ -1,7 +1,7 @@
 
 
-n = 100
-def dbl_linear(n):
+# this one is too complex
+def dbl_linear_version(n):
     u=[1]
     if n == 0:
         return u
@@ -14,4 +14,26 @@ def dbl_linear(n):
         return u
 
 m = dbl_linear(n)
-print(dbl_linear(n))        
+     
+
+
+def dbl_linear(n):
+    u = [1]
+    x,y = 0,0
+    if n == 0 :
+        return 0
+    else:
+        while len(u) <= n:
+            a,b=2*u[x]+1,3*u[y]+1
+            if a == b :
+                u.append(a)
+                x += 1 
+                y += 1
+            elif a>b:
+                u.append(b)
+                y += 1
+            elif b>a:
+                u.append(a)
+                x += 1   
+            
+        return u[n]
