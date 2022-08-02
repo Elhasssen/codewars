173cb9ad22519fbacde278c48530f4f6fc84f17b
ElHassen <Boumeddiene.elhassen@gmail.com>
2022-08-02 07:23:46 +0100

    range extraction solved a problem

--- range_extraction.py
+++ range_extraction.py
@@ -1,25 +1,17 @@
-theList = [-10, -9, -8, -6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20]
-
-theRange = ''
-theMiniRange = [theList[0]]
-begin = 0
-
-for i in range(1,len(theList)):
-    if theList[begin] + 1 == theList[i]:
-        theMiniRange.append(theList[i])
-        begin = i
-        
-    else :
-        if len(theMiniRange) != 1 :
-            theRange = theRange + str(theMiniRange[0]) + '-' + str(theMiniRange[-1]) + ','
-            begin = i
-            theMiniRange = [theList[i]]
-        elif len(theMiniRange) == 1 :
-            theRange = theRange + str(theMiniRange[0]) + ','
-            begin = i
-            theMiniRange = [theList[i]]
-    print(f'the begin is {begin}')
-    print(f'the i value is {i}')
-    print(theMiniRange)
-    print(theRange)
-
+def solution(args):
+    theRange = ''
+    i = 0
+    while i < len(args):
+        begin = args[i]
+        while i < len(args) - 1 and args[i] + 1 == args[i + 1] :
+            i += 1
+        end = args[i]
+        if end - begin >= 2:
+            theRange = theRange + str(begin) + '-' + str(end) + ','
+        if end - begin == 1 :
+            theRange = theRange + str(begin) + ',' + str(end) + ','
+        if end - begin == 0 :
+            theRange = theRange + str(begin) + ','
+        i += 1
+    theRange = theRange[:-1]
+    return theRange
